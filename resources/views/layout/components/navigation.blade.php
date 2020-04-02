fe4c0cc252614728df2d6437924b0979c0da3bf0
Fadillah <fadillahfadlurrahman@gmail.com>
2020-04-03 06:03:47 +0700

    landing page

--- resources/views/layout/components/navigation.blade.php
+++ resources/views/layout/components/navigation.blade.php
@@ -1,16 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary p-2">
-<div class="container">
-  <div class="row"><img src="/img/smk.png" class="mr-3" width="11%"><h3 class="text-white" style="font-family:Moon;margin-top:36px;">SMK Negeri 4 Bandung</h3></div>
+  <div class="container">
+  <div class="row"><img src="/img/logo.png" class="mr-3" width="16%" height="9%"><h3 class="text-white" style="font-family:arial;margin-top:12px;"></h3></div>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <center>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link @yield('global')" href="{{ route('global') }}"><h6>Data Global</h6></a>
-      <a class="nav-item nav-link @yield('indonesia')" href="{{ route('indonesia') }}"><h6>Data Indonesia</h6></a>
-    </div>
+    <ul class="navbar-nav mr-3">
+      <li class="nav-item dropdown mx-2">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Gallery
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown mx-2">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+      <li class="nav-item dropdown mx-2">
+        <a href="#" class="nav-link">About Us</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2  mx-2">
+      <button class="btn btn-light rounded-pill my-2 px-5 my-sm-0" type="submit">Login</button>
+    </form>
   </div>
-  </center> 
-</div>
+  </div>
 </nav>
